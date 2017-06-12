452f45edadb73ead9dc62eb5ee5199433c697c6d
Patryk Wlazłowicz <patrykwlazlowicz@icloud.com>
2017-06-12 11:37:06 +0200

    Bugfixing 1

--- server/service/message-service.js
+++ server/service/message-service.js
@@ -1,3 +1,6 @@
+// CUSTOM MODULES
+const ConnectionInfo = require('../util/connection-info.js');
+
 var MessageDAO = null;
 var MessageService = null;
 
@@ -8,11 +11,11 @@ module.exports = function(mongoose) {
         from: String,
         time: String,
         msg: String,
-        readed: Boolean
+        unread: Boolean
     });
     MessageService = {
         getHistory: function(forUser, res) {
-            Message.find({
+            MessageDAO.find({
                 $and: [
                     {
                         $or: [
@@ -22,10 +25,6 @@ module.exports = function(mongoose) {
                                 'reciver2': forUser
                             }
                         ]
-                    }, {
-                        $ne: {
-                            'from': forUser
-                        }
                     }
                 ]
             }, function(err, history) {
@@ -34,14 +33,14 @@ module.exports = function(mongoose) {
                     console.log(err);
                 } else {
                     var unreadMsg = {};
-                    for (var registry in history) {
-                        var client = registry.reciver1 === forUser
-                            ? registry.reciver2
-                            : registry.reciver1;
+                    for (var i in history) {
+                        var client = history[i].reciver1 === forUser
+                            ? history[i].reciver2
+                            : history[i].reciver1;
                         if (!(client in unreadMsg)) {
                             unreadMsg[client] = 0;
                         }
-                        if(registry.unread === true){
+                        if (history[i].unread && history[i].from !== forUser) {
                             unreadMsg[client]++;
                         }
                     }
@@ -53,7 +52,66 @@ module.exports = function(mongoose) {
                 res.end();
             });
         },
-        getTalk: function() {}
+        getTalk: function(forUser, withClient, res) {
+            var reciver1 = withClient < forUser
+                ? withClient
+                : forUser;
+            var reciver2 = withClient < forUser
+                ? forUser
+                : withClient;
+            MessageDAO.update({
+                'reciver1': reciver1,
+                'reciver2': reciver2,
+                'from': {
+                    $ne: forUser
+                },
+                'unread': true
+            }, {
+                $set: {
+                    'unread': false
+                }
+            }, {
+                multi: true
+            }, function(err, updated) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    MessageDAO.find({
+                        'reciver1': reciver1,
+                        'reciver2': reciver2
+                    }, 'msg time from', function(err, msgs) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            res.json(msgs);
+                            res.end();
+                            ConnectionInfo.connections[forUser].talkWith = withClient;
+                            ConnectionInfo.connections[forUser].emit('refreshHistory');
+                        }
+                    });
+                }
+            });
+        },
+        createMassage: function(host, client, from, time, msg) {
+            var reciver1 = client < host
+                ? client
+                : host;
+            var reciver2 = client < host
+                ? host
+                : client;
+            MessageDAO.create({
+                reciver1: reciver1,
+                reciver2: reciver2,
+                from: host,
+                time: time,
+                msg: msg,
+                unread: !(client in ConnectionInfo.connections) || (client in ConnectionInfo.connections && ConnectionInfo.connections[client].talkWith !== host)
+            }, function(err, msg) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        }
     }
     return MessageService;
 };
